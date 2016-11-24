4fd19f7501e4e08dd34cd848f3191196f6e1c73a
ikrong <lun.i@outlook.com>
2016-11-24 22:57:57 +0800

    add more

---
+++ console.js
@@ -0,0 +1,55 @@
+var ws_link = "ws://localhost:8080";
+
+//将浏览器console发送至服务器
+var send_log = function () { };
+//处理服务器需求
+var deal_server_code = function (string) {
+	eval(JSON.parse(string));
+};
+
+var ws;
+if (WebSocket) {
+	ws = new WebSocket(ws_link, "echo-protocol");
+	ws.onopen = function () {
+		send_log = function (method, args) {
+			ws.send(JSON.stringify({
+				method: method,
+				args: args
+			}))
+		}
+	}
+	ws.onmessage = function (msg) {
+		var result = eval(msg.data);
+		send_log("log", [result]);
+	}
+}
+
+// Injected Ionic CLI Console Logger
+(function () {
+	var methods = "assert clear count debug dir dirxml error exception group groupCollapsed groupEnd info log markTimeline profile profileEnd table time timeEnd timeStamp trace warn".split(" ");
+	var console = (window.console = window.console || {});
+	var logCount = 0;
+	window.onerror = function (msg, url, line) {
+		if (msg && url) console.error(msg, url, (line ? "Line: " + line : ""));
+	};
+	function sendConsoleLog(method, args) {
+		try {
+			send_log(method, args)
+			// var xhr = new XMLHttpRequest();
+			// xhr.open("POST", "/__ionic-cli/console", true);
+			// xhr.send(JSON.stringify({ index: logCount, method: method, ts: Date.now(), args: args }));
+			// logCount++;
+		} catch (e) { }
+	}
+	for (var x = 0; x < methods.length; x++) {
+		(function (m) {
+			var orgConsole = console[m];
+			console[m] = function () {
+				try {
+					sendConsoleLog(m, Array.prototype.slice.call(arguments));
+					if (orgConsole) orgConsole.apply(console, arguments);
+				} catch (e) { }
+			};
+		})(methods[x]);
+	}
+} ());
--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-var WebSocketServer = require('websocket').server;
+var WSServer = require('websocket').server;
 var http = require('http');
+var readline = require('readline');
 
-var server = http.createServer(function(request, response) {
+//创建readline接口实例
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var deal_input = function () { };
+
+// question方法
+var waiting_input = function (showdetail) {
+    rl.question(showdetail ? "请输入调试信息:>>" : ">>", function (answer) {
+        if (answer.trim()) {
+            switch (answer.trim()) {
+                case "quit":
+                    rl.close();
+                    break;
+                case "q":
+                    rl.close();
+                    break;
+                default:
+                    deal_input(answer.trim());
+                    waiting_input();
+            }
+        } else {
+            waiting_input();
+        }
+    });
+}
+
+// close事件监听
+rl.on("close", function () {
+    // 结束程序
+    process.exit(0);
+});
+
+var server = http.createServer(function (request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-wsServer = new WebSocketServer({
+wsServer = new WSServer({
     httpServer: server,
     // You should not use autoAcceptConnections for production
     // applications, as it defeats all standard cross-origin protection
@@ -21,31 +57,33 @@ wsServer = new WebSocketServer({
 });
 
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
 }
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
 
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    deal_input = function (text) {
+        connection.sendUTF(text);
+    }
+    waiting_input(1);
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+            console.log(message.utf8Data);
         }
         else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            console.log("binary file isn't support!");
         }
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
